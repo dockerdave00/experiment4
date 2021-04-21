1972c784223a9afa065d1bfd3ff3b2826f40b0a1
dockerdave00 <82287421+dockerdave00@users.noreply.github.com>
2021-04-20 20:25:23 -0600

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+  
+# start from base
+FROM python:3
+
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+EXPOSE 5000
+
+CMD [ "python", "./app.py" ]
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+import redis
+
+app = Flask(__name__)
+
+@app.route("/setname")
+def setname():
+    value = request.args.get('name')
+    if not value:
+        return 'Missing required input, 400\n'
+    else:
+        r = redis.Redis(host='172.17.0.1', port=6379)
+        s = r.set('name', value, ex=30)
+
+        if s == True:
+            return 'Cache set successful\n'
+        else:
+            return 'Cache set failure\n'
+
+@app.route("/hello")
+def hello():
+
+    r = redis.Redis(host='172.17.0.1', port=6379, decode_responses=True)
+    s = r.get('name')
+
+    if not s:
+        s = "World"
+
+    return 'Hello, ' + s  + '!\n'
+     
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
