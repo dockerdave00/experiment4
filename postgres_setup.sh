575065a7424f38bd954ade2dd5e9972108ec2527
Dave Norris <dockerdave00@gmail.com>
2021-05-04 20:52:31 -0600

    fixed potential bug, added SERIAL primary key feature

--- postgres_setup.sh
+++ postgres_setup.sh
@@ -11,7 +11,7 @@ EXEC="docker exec"
 echo "Check if postgres container is running"
 postgres_container_id=$(docker ps -a | grep postgres_db | awk '{print $1}')
 
-if [ ${postgres_container_id} ]; then
+if [ "${postgres_container_id}" ]; then
 
 	echo "Stopping postgres container"
 	result1=$(${STOP} postgres_db)
@@ -53,7 +53,7 @@ sleep 1 # give docker/psql time to complete
 
 echo "Creating 'users' table in 'hello' database"
 result5=$(${EXEC} -it postgres_db psql --username postgres -d hello -a -c \
-	'CREATE TABLE users (id int, name varchar, address varchar, phone varchar, PRIMARY KEY(id));')
+	'CREATE TABLE users (id SERIAL PRIMARY KEY, name varchar, address varchar, phone varchar);')
 # modify command result output down to simple, usable string
 result5_mod=$(echo ${result5} | tr '\r\n' ' ' | awk -F\; '{print $2}' | xargs)
 if [ "${result5_mod}" != "CREATE TABLE" ]; then
@@ -64,21 +64,29 @@ sleep 1 # give docker/psql time to complete
 
 echo "Populating a row of 'users' table in 'hello' database"
 result6=$(${EXEC} -it postgres_db psql --username postgres -d hello -a -c \
-	"INSERT INTO users(id, name, address, phone) VALUES (1, 'Teresa', '8745 W Geddes Place', '303-594-9683');")
-	#"INSERT INTO users(id, name, address, phone) VALUES (1, 'Dave', '8745 W Geddes Place', '303-204-6735');")
+	"INSERT INTO users(name, address, phone) VALUES ('Teresa', '8745 W Geddes Place', '303-594-9683');")
 # modify command result output down to simple, usable string
 result6_mod=$(echo ${result6} | tr '\r\n' ' ' | awk -F\; '{print $2}' | xargs)
 if [ "${result6_mod}" == "INSERT 0 1" ]; then
 	echo -e "\t** Value store successful **"
 else
-	echo "\tValue store failure"
+	echo -e "\tValue store failure"
+	exit 1
+fi
+
+echo "Populating next row of 'users' table in 'hello' database"
+result7=$(${EXEC} -it postgres_db psql --username postgres -d hello -a -c \
+	"INSERT INTO users(name, address, phone) VALUES ('Dave', '8745 W Geddes Place', '303-204-6735');")
+# modify command result output down to simple, usable string
+result7_mod=$(echo ${result7} | tr '\r\n' ' ' | awk -F\; '{print $2}' | xargs)
+if [ "${result7_mod}" == "INSERT 0 1" ]; then
+	echo -e "\t** Value store successful **"
+else
+	echo -e "\tValue store failure"
 	exit 1
 fi
 
 echo -e "\nDisplaying contents of 'hello' database, 'users' table\n"
 ${EXEC} -it postgres_db psql --username postgres -d hello -a -c "SELECT * FROM users;"
 
-
-
-
-
+exit
